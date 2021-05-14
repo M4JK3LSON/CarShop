793daa49f469b6338816d6997807fa9ac62005d6
Michal L <Michal@imac-programista-2.local>
2021-05-14 05:55:37 +0200

    Initial Commit

---
+++ Lab6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab6
+//
+//  Created by Michal L on 14.05.2021.
+//  Copyright © 2021 Michal L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
